a61c00550aa1898118b4eb1f3a883e88ec724158
sufian92 <sufianamara92@gmail.com>
2019-07-07 15:48:17 +0200

    results page object
    adding results page object

--- src/main/java/testco/qa/automation/pageobjects/AutomationPracticeResultsPage.java
+++ src/main/java/testco/qa/automation/pageobjects/AutomationPracticeResultsPage.java
@@ -1,5 +1,28 @@
 package testco.qa.automation.pageobjects;
 
-public class AutomationPracticeResultsPage {
+import testco.qa.automation.helpers.SeleniumHelper;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class AutomationPracticeResultsPage extends AutomationPracticeBasePage {
+
+	@FindBy(how = How.CSS, using = ".page-heading.product-listing")
+	private WebElement lblHeading;
+
+
+	public AutomationPracticeResultsPage(WebDriver driver) {
+
+		super(driver);
+
+		this.driver = driver;
+
+		PageFactory.initElements(driver, this);
+		
+		SeleniumHelper.waitForPageLoad(this.driver,lblHeading);
+	}
+
 
 }
